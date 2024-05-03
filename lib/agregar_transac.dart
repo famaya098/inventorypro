905774e790d558926ab96edfae44aa0e804a98e5
famaya098 <freddyzyf@gmail.com>
2024-05-02 22:25:43 -0600

    Diseño de pantalla de agregar transac y productos carrito

--- lib/agregar_transac.dart
+++ lib/agregar_transac.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:inventorypro/productos_carrito.dart';
 import 'my_drawer.dart';
 
 class AgregarTransac extends StatelessWidget {
@@ -34,7 +35,7 @@ class AgregarTransac extends StatelessWidget {
             },
           ),
         ],
-        backgroundColor: const Color(0xFF027A70), 
+        backgroundColor: const Color(0xFF027A70),
         elevation: 0,
         systemOverlayStyle: SystemUiOverlayStyle.light,
       ),
@@ -47,7 +48,7 @@ class AgregarTransac extends StatelessWidget {
             const Center(
               child: Text(
                 'VENTAS Y COMPRAS',
-               style: TextStyle(
+                style: TextStyle(
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
                   color: Color(0xFF027A70),
@@ -55,27 +56,180 @@ class AgregarTransac extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 20),
-            _buildTextFormField(label: 'Código Transacción:', hintText: ''),
+            _buildTextFormField(
+                label: 'Código Transacción:', hintText: ''),
             const SizedBox(height: 20),
-            _buildTextFormField(label: 'Fecha:', hintText: '', keyboardType: TextInputType.datetime),
+            _buildTextFormField(
+                label: 'Fecha:', hintText: '', keyboardType: TextInputType.datetime),
             const SizedBox(height: 20),
-            _buildDropdownButton(label: 'Medicamento:', items: ['Medicamento 1', 'Medicamento 2', 'Medicamento 3']),
+            _buildDropdownButton(
+                label: 'Tipo Transacción:', items: ['Entrada', 'Salida']),
             const SizedBox(height: 20),
-            _buildTextFormField(label: 'Stock:', hintText: '', keyboardType: TextInputType.number),
+            // Campo de búsqueda de producto
+           Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    // Navegar a la pantalla ProductosCarrito cuando se presiona el botón
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ProductosCarrito()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF027A70),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  icon: const Icon(Icons.search),
+                  label: const Text(
+                    'Agregar Producto',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            // Fin del campo de búsqueda
             const SizedBox(height: 20),
-            _buildTextFormField(label: 'Cantidad de Ingreso o Salida:', hintText: '', keyboardType: TextInputType.number),
+            // Aquí incluimos el ListView.builder antes del botón "Guardar"
+            Container(
+              height: 300, // Altura del contenedor para la lista vertical
+              child: ListView.builder(
+                padding: EdgeInsets.zero,
+                itemCount: 5, // Cambia este valor según la cantidad de elementos que desees mostrar
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 4,
+                            color: Colors.grey.withOpacity(0.5),
+                            offset: const Offset(0, 1),
+                          ),
+                        ],
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  width: 80,
+                                  height: 80,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(12),
+                                    color: Colors.grey[300],
+                                  ),
+                                  child: const Icon(Icons.image),
+                                ),
+                                const SizedBox(width: 12),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Nombre del producto $index',
+                                        style: const TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 4),
+                                      const Text(
+                                        'Precio unitario: \$10.00',
+                                        style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 4),
+                                      const Text(
+                                        'Precio total: \$10.00',
+                                        style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                      const Text(
+                                        'Cantidad: 1',
+                                        style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            // Fin de la inserción del ListView.builder
             const SizedBox(height: 20),
-            _buildDropdownButton(label: 'Tipo Transacción:', items: ['Entrada', 'Salida']),
-            const SizedBox(height: 20),
-            _buildTextFormField(label: 'Total Stock:', hintText: '', keyboardType: TextInputType.number),
+            // Contenedor de precios...
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: const Color(0xFF027A70), // Color del botón "Guardar"
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Precio:',
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+                      ),
+                      Text(
+                        '\$100.00',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Precio con IVA:',
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+                      ),
+                      Text(
+                        '\$116.00',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            // Fin del contenedor de precios
             const SizedBox(height: 20),
             Center(
               child: ElevatedButton(
                 onPressed: () {
-                  
+                  // Lógica para guardar
                 },
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF027A70), 
+                  backgroundColor: const Color(0xFF027A70),
                   padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(30),
@@ -93,7 +247,8 @@ class AgregarTransac extends StatelessWidget {
     );
   }
 
-  Widget _buildTextFormField({required String label, required String hintText, TextInputType? keyboardType}) {
+  Widget _buildTextFormField(
+      {required String label, required String hintText, TextInputType? keyboardType}) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -132,7 +287,7 @@ class AgregarTransac extends StatelessWidget {
             height: 2,
             color: const Color(0xFF027A70),
           ),
-          icon: const Icon(Icons.arrow_drop_down, color: Colors.white), 
+          icon: const Icon(Icons.arrow_drop_down, color: Colors.white),
           iconSize: 36,
           style: const TextStyle(fontSize: 16, color: Colors.black87),
           items: items.map((String value) {
@@ -146,4 +301,10 @@ class AgregarTransac extends StatelessWidget {
       ],
     );
   }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: AgregarTransac(),
+  ));
 }
--- lib/productos_carrito.dart
+++ lib/productos_carrito.dart
@@ -1,116 +1,180 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:inventorypro/agregar_transac.dart';
-import 'my_drawer.dart'; 
+import 'my_drawer.dart';
+import 'home_screen.dart'; // Importa la pantalla HomeScreen
 
 class ProductosCarrito extends StatelessWidget {
-  const ProductosCarrito({super.key});
+  const ProductosCarrito({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Buscar Productos'),
-        leading: Builder(
-          builder: (context) {
-            return IconButton(
-              icon: const Icon(
-                Icons.menu,
-                color: Colors.black87,
-              ),
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              },
+        title: const Text('InventoryPro'),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const AgregarTransac()),
             );
           },
         ),
-        
+        actions: [
+          Builder(
+            builder: (context) {
+              return IconButton(
+                icon: const Icon(
+                  Icons.menu,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+              );
+            },
+          ),
+        ],
+        backgroundColor: const Color(0xFF027A70), // Color de la AppBar
+        elevation: 0,
+        systemOverlayStyle: SystemUiOverlayStyle.light,
       ),
       drawer: const MyDrawer(),
-      body: CustomScrollView(
-        slivers: [
-          const SliverPadding(
-            padding: EdgeInsets.all(8.0),
-            sliver: SliverToBoxAdapter(
-              child: Center(
-                child: Text(
-                  'LISTA DE PRODUCTOS',
-                  style: TextStyle(fontSize: 24),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Center(
+              child: Text(
+                'LISTA DE PRODUCTOS',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF027A70),
                 ),
               ),
-            )
-          ),
-          SliverPadding(
-            padding: const EdgeInsets.all(8.0),
-            sliver: SliverToBoxAdapter(
-              child: Row(
+            ),
+            const SizedBox(height: 20),
+            // Listado de productos
+            _buildProductList(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProductList() {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: 5, // Aquí deberías tener la cantidad real de productos
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 4,
+                  color: Colors.grey.withOpacity(0.5),
+                  offset: const Offset(0, 1),
+                ),
+              ],
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Buscar producto',
-                        prefixIcon: Icon(Icons.search),
+                  Row(
+                    children: [
+                      Container(
+                        width: 80,
+                        height: 80,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.grey[300],
+                        ),
+                        child: const Icon(Icons.image),
                       ),
-                    ),
+                      const SizedBox(width: 12),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Nombre del producto $index',
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 4),
+                            const Text(
+                              'Código: 12345',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            const Text(
+                              'Precio Compra: \$50.00',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            const Text(
+                              'Precio Venta: \$100.00',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            const Text(
+                              'Stock: 10 unidades',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            const Text(
+                              'Unidad: Libras',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.shopping_cart),
+                        onPressed: () {
+                          // Acción al presionar el icono de carrito
+                        },
+                      ),
+                    ],
                   ),
-                  const SizedBox(width: 8),
-                  ElevatedButton(
-                    onPressed: () {
-                      
-                    },
-                    child: const Text('Buscar'),
-                  ),
-                  
                 ],
               ),
             ),
           ),
-          const SliverToBoxAdapter(
-            child: Divider(),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return ListTile(
-                  title: Text('Nombre del producto ${index + 1}'),
-                  subtitle: const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Código: 12345'),                      
-                      Text('Precio Venta: \$100.00'),    
-                      Text('Stock: 10 unidades'),                                          
-                      
-                    ],
-                  ),
-                  trailing: GestureDetector(
-                    
-                            child: const IconButton(
-                      icon: Icon(Icons.local_grocery_store),
-                      onPressed: null,
-                           ),
-                  ),
-                );
-              },
-              childCount: 4,
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              alignment: Alignment.centerRight,
-              child: 
-             OutlinedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const AgregarTransac()),
-                      );
-                    },
-                    child: const Text('Cerrar'),
-                  ),
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: ProductosCarrito(),
+  ));
 }
